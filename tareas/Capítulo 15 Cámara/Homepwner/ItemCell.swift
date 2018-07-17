e151cb592acc058b9e057d0a03e42a1c794c38e2
Adrian <adrian.hidalgo1494@gmail.com>
2018-07-16 20:00:33 -0500

    eliminar
    delete

---
+++ tareas/Capítulo 15 Cámara/Homepwner/ItemCell.swift
@@ -0,0 +1,18 @@
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var serialNumberLabel: UILabel!
+    @IBOutlet var valueLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        nameLabel.adjustsFontForContentSizeCategory = true
+        serialNumberLabel.adjustsFontForContentSizeCategory = true
+        valueLabel.adjustsFontForContentSizeCategory = true
+    }
+    
+}
